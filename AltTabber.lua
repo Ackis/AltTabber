966c92f9ecd2d79c1ea1aed7660049a3a9499e5f
John Pasula <ackis@shaw.ca>
2008-07-20 20:41:47 +0000

    - Updated download count - Added ability to localize - Fixed externals - Removed a piece of unneeded code - Version 1.2

--- AltTabber.lua
+++ AltTabber.lua
@@ -1,22 +1,88 @@
 --[[
 ****************************************************************************************
 Alt-Tabber
+
 $Date$
 $Rev$
 
-Author: Ackis on Illidan US Horde
+****************************************************************************************
+
+Please see Wowace.com for more information.
+
 ****************************************************************************************
 ]]
 
-AltTabber = LibStub("AceAddon-3.0"):NewAddon("AltTabber", "AceConsole-3.0", "AceEvent-3.0")
+AltTabber 		= LibStub("AceAddon-3.0"):NewAddon("AltTabber", "AceConsole-3.0", "AceEvent-3.0")
 
 local addon = AltTabber
-local AceConfig = LibStub("AceConfig-3.0")
+
+-- Localization
+do
+	-- enUS stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "enUS", true)
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- enGB stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "enGB")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- deDE stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "deDE")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- frFR stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "frFR")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- zhCN stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "zhCN")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- zhTW stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "zhTW")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- koKR stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "koKR")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- esES stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "esES")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+	-- esMX stuff
+	local L = LibStub("AceLocale-3.0"):NewLocale("AltTabber", "esMX")
+	if L then
+		L["BGSNDON"] = "Enabling sound in background so you can hear ready checks while alt-tabbed."
+	end
+
+end
+
+local L = LibStub("AceLocale-3.0"):GetLocale("AltTabber", false)
+
 local GetCVar = GetCVar
 local PlaySoundFile = PlaySoundFile
 
 function addon:OnInitialize()
+
 	LibStub("LibAboutPanel").new(nil, "AltTabber")
+
 end
 
 function addon:OnEnable()
@@ -30,7 +96,7 @@ function addon:READY_CHECK()
 	-- Abuses a bug? in that PlaySoundFile will still play
 	if (GetCVar("Sound_EnableSFX") == "0") then
 		if (GetCVar("Sound_EnableSoundWhenGameIsInBG") == "0") then
-			self:Print("Enabling sound in background so you can hear ready checks while alt-tabbed.")
+			self:Print(L["BGSNDON"])
 			SetCVar("Sound_EnableSoundWhenGameIsInBG", "1")
 		end
 		PlaySoundFile("Sound\\interface\\ReadyCheck.wav")
