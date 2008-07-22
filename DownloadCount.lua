d1367bf1b03f79746825a4aaba9e04058da3dfb6
John Pasula <ackis@shaw.ca>
2008-07-22 21:57:22 +0000

    - Updated download count - Dry-coded (compiles w/ luac -p)

--- DownloadCount.lua
+++ DownloadCount.lua
@@ -1,6 +1,6 @@
 --[[
 ****************************************************************************************
-AckisRecipeList Download Count
+Download Count
 $Date$
 $Rev$
 
@@ -11,63 +11,17 @@ Author: Ackis on Illidan US Horde
 Require socket.  You can get it from: http://luaforge.net/projects/luasocket/
 
 This is not to be run in WoW.  It will query all of the release sites and obtain download information for
-Ackis Recipe List
+the mod specified in the variables.
 
 ****************************************************************************************
 ]]--
 
 http = require("socket.http")
 
-function GetCurseDownloads()
-
-	local addonurl = "http://www.curse.com/downloads/details/12774"
-	local patternmatch1 = "<th>Downloads Total:</th>\n                                            <td>(%d+,%d+)</td>"
-	local patternmatch2 = "<th>Downloads Total:</th>\n                                            <td>(%d+)</td>"
+function GetNumberDownloads(addonurl,patternmatch1,patternmatch2)
 
 	local _,_,temp = string.find(http.request(addonurl), patternmatch1)
 	local _,_,temp1 = string.find(http.request(addonurl), patternmatch2)
-
-	local numberdownloads
-
-	if (temp) then
-		local numberdownloads = string.gsub(temp,",","")
-		return numberdownloads
-	else
-		return temp1
-	end
-
-end
-
-function GetWoWUIDownloads()
-
-	local addonurl = "http://wowui.worldofwar.net/?p=mod&m=6460"
-	local patternmatch1 = "<b>(%d+,%d+)</b> total downloads</b>"
-	local patternmatch2 = "<b>(%d+)</b> total downloads</b>"
-
-	local _,_,temp = string.find(http.request(addonurl), patternmatch1)
-	local _,_,temp1 = string.find(http.request(addonurl), patternmatch2)
-
-	local numberdownloads
-
-	if (temp) then
-		local numberdownloads = string.gsub(temp,",","")
-		return numberdownloads
-	else
-		return temp1
-	end
-
-end
-
-function GetWoWIDownloads()
-
-	local addonurl = "http://www.wowinterface.com/downloads/info9197-AltTabber.html"
-	local patternmatch1 = "<td class=\"alt1\"><div class=\"smallfont\">(%d+,%d+)</div></td>"
-	local patternmatch2 = "<td class=\"alt1\"><div class=\"smallfont\">(%d+)</div></td>"
-
-	local _,_,temp = string.find(http.request(addonurl), patternmatch1)
-	local _,_,temp1 = string.find(http.request(addonurl), patternmatch2)
-
-	local numberdownloads
 
 	if (temp) then
 		local numberdownloads = string.gsub(temp,",","")
@@ -79,9 +33,23 @@ function GetWoWIDownloads()
 end
 
 do
-	local curse = GetCurseDownloads()
-	local WoWUI = GetWoWUIDownloads()
-	local WoWI = GetWoWIDownloads()
+
+	-- URL of where the add-ons are hosted
+	local curseurl = "http://www.curse.com/downloads/details/12774"
+	local wowuiurl = "http://wowui.worldofwar.net/?p=mod&m=6460"
+	local wowiurl = "http://www.wowinterface.com/downloads/info9197-AltTabber.html"
+
+	-- Patterns which we match to find download information
+	local cursepatternmatch1 = "<th>Downloads Total:</th>\n                                            <td>(%d+,%d+)</td>"
+	local cursepatternmatch2 = "<th>Downloads Total:</th>\n                                            <td>(%d+)</td>"
+	local wowuipatternmatch1 = "<b>(%d+,%d+)</b> total downloads</b>"
+	local wowuipatternmatch2 = "<b>(%d+)</b> total downloads</b>"
+	local wowipatternmatch1 = "<td class=\"alt1\"><div class=\"smallfont\">(%d+,%d+)</div></td>"
+	local wowipatternmatch2 = "<td class=\"alt1\"><div class=\"smallfont\">(%d+)</div></td>"
+
+	local curse = GetNumberDownloads(curseurl,cursepatternmatch1,cursepatternmatch2)
+	local WoWUI = GetNumberDownloads(wowuiurl,wowuipatternmatch1,wowuipatternmatch2)
+	local WoWI = GetNumberDownloads(wowiurl,wowipatternmatch1,wowipatternmatch2)
 
 	print("Downloads by site:")
 	print("Curse Gaming:   " .. curse)
